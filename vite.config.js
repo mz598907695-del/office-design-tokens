f0810d67a87241d72859d5f31cb1fd34506de345
Yangminghze <yangmingzhe03@meituan.com>
2026-03-29 17:35:58 +0800

    chore: 添加部署配置，支持动态资源路径

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 
+const PUBLIC_PATH = process.env.PUBLIC_PATH || '/';
+
 export default defineConfig({
   root: '.',
+  base: PUBLIC_PATH,
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
